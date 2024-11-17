1a2f749a3412e83ba87c40dcbb3ce6707af1423c
niclaus <rendonniclaus111@gmail.com>
2024-11-17 16:27:55 +0800

    Part Exercises

---
+++ part09-Part09_05.TacoBoxes/src/main/java/CustomTacoBox.java
@@ -0,0 +1,21 @@
+public class CustomTacoBox implements TacoBox {
+    private int tacos;
+
+    public CustomTacoBox(int tacos) {
+        this.tacos = tacos;
+    }
+
+    @Override
+    public void eat() {
+        if (this.tacos > 0) {
+            this.tacos--;
+        }
+
+    }
+
+    @Override
+    public int tacosRemaining() {
+        return this.tacos;
+    }
+
+}
