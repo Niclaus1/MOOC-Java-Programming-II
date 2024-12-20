cbf9c4f6907577c4b411fbf94c54a6937a1ea368
niclaus <rendonniclaus111@gmail.com>
2024-12-20 23:52:31 +0800

    Part Exercises

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,16 @@
+public class Bot extends Player {
+
+    @Override
+    public void play() {
+        // TODO Auto-generated method stub
+        super.play();
+    }
+
+    @Override
+    public void printName() {
+        // TODO Auto-generated method stub
+        super.printName();
+    }
+
+
+}
